0a8ee251c6351e8340227dace11ad9b9c70361b0
Peter Pagac <QuarKUS7@gmail.com>
2019-04-30 11:18:43 +0200

    test if main is not needed

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def upload():
 def classification(result):
     return render_template('result.html', result=result)
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+#if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
